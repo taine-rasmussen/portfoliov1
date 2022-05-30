b2939b3fa63470c9f162afc4261c46a0c9d8f94f
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-05-30 20:53:25 +1200

    links open in new tab

--- src/Components/Header/Socials.js
+++ src/Components/Header/Socials.js
@@ -4,8 +4,8 @@ import { BsLinkedin, BsGithub } from 'react-icons/bs'
 const Socials = () => {
    return (
       <div className="header_socials">
-         <a href='https://github.com/taine-rasmussen'><BsGithub /></a>
-         <a href='https://www.linkedin.com/in/taine-rasmussen-a66a86184/'><BsLinkedin /></a>
+         <a href='https://github.com/taine-rasmussen' target='_blank'><BsGithub /></a>
+         <a href='https://www.linkedin.com/in/taine-rasmussen-a66a86184/' target='_blank'><BsLinkedin /></a>
       </div>
    )
 }
