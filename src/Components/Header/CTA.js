aa7ec0275d6c8a4bd641bf2bf4a8015d251a43c0
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-05-30 20:43:51 +1200

    started header component

---
+++ src/Components/Header/CTA.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CTA = () => {
+   return (
+      <div className="cta">
+         <a download>Download CV</a>
+         <a href='#contact'>Let's Talk</a>
+      </div>
+   )
+}
+
+export default CTA
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import './Header.css'
+import CTA from './CTA'
 
 const Header = () => {
-   return (
-      <div>
-         Header
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hello I'm</h5>
+        <h1>Taine Rasmussen</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
       </div>
-   )
+    </header>
+  )
 }
 
 export default Header
