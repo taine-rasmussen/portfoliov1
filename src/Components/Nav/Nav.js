f14b55dc7dd52edae69e7c16f69219f51dc139e7
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-05-31 20:56:46 +1200

    added icons for nav links

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Nav.css'
-import { AiOutlineHome } from 'react-icons/ai';
+import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai';
+import { BiBook, BiMessageSquareDetail } from 'react-icons/bi';
+
 
 const Nav = () => {
    return (
       <nav>
          <a href='#'><AiOutlineHome /></a>
+         <a href='#about'><AiOutlineUser /></a>
+         <a href='#experience'><BiBook /></a>
+         <a href='#contact'><BiMessageSquareDetail /></a>
       </nav>
    )
 }
