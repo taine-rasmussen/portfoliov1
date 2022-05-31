73bc5492abb6f290dee94c7540fadf739fe1f7ef
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-05-31 21:19:14 +1200

    useState to manage changing of navActive class

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,18 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Nav.css'
 import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai';
 import { BiBook, BiMessageSquareDetail } from 'react-icons/bi';
 
 
 const Nav = () => {
-   return (
-      <nav>
-         <a href='#' className="active"><AiOutlineHome /></a>
-         <a href='#about'><AiOutlineUser /></a>
-         <a href='#experience'><BiBook /></a>
-         <a href='#contact'><BiMessageSquareDetail /></a>
-      </nav>
-   )
+  const [activeNav, setActiveNav] = useState('#')
+
+
+  return (
+    <nav>
+      <a
+        href='#'
+        onClick={() => setActiveNav('#')}
+        className={activeNav === '#' ? 'active' : ''}
+      ><AiOutlineHome /></a>
+      <a
+        href='#about'
+        onClick={() => setActiveNav('about')}
+        className={activeNav === 'about' ? 'active' : ''}
+      ><AiOutlineUser /></a>
+      <a
+        href='#experience'
+        onClick={() => setActiveNav('experience')}
+        className={activeNav === 'experience' ? 'active' : ''}
+      >
+        <BiBook /></a>
+      <a
+        href='#contact'
+        onClick={() => setActiveNav('contact')}
+        className={activeNav === 'contact' ? 'active' : ''}
+      ><BiMessageSquareDetail /></a>
+    </nav>
+  )
 }
 
 export default Nav
