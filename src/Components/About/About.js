c063a0489151f8808fac3dc4cef1110b108f3d8c
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-06-01 21:13:16 +1200

    added react icon

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,27 +1,34 @@
 import React from 'react'
 import './About.css'
 import profileImg from '../../assets/profile.jpeg'
+import { FaAward } from 'react-icons/fa'
 
 const About = () => {
-   return (
-      <section id='about'>
-         <h5>Get to know</h5>
-         <h2>About me</h2>
+  return (
+    <section id='about'>
+      <h5>Get to know</h5>
+      <h2>About me</h2>
 
-         <div className='container about_container'>
-            <div className='about_me'>
-               <div className='about_me_image'>
-                  <img src={profileImg} alt='Profile Image' />
-               </div>
+      <div className='container about_container'>
+        <div className='about_me'>
+          <div className='about_me_image'>
+            <img src={profileImg} alt='Profile Image' />
+          </div>
 
-            </div>
-            <div className='about_content'>
+        </div>
+        <div className='about_content'>
+          <div className='about_cards'>
+            <article className='about_card'>
+              <FaAward />
+              <h5>experience</h5>
+              <small>1 year working </small>
+            </article>
 
-            </div>
-
-         </div>
-      </section>
-   )
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default About
