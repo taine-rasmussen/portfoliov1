2bd39f6f0400a2e7baee780b458e6d78cf83be77
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-06-01 21:08:14 +1200

    added markup for about section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,7 +4,21 @@ import './About.css'
 const About = () => {
    return (
       <section id='about'>
-         About
+         <h5>Get to know</h5>
+         <h2>About me</h2>
+
+         <div className='container about_container'>
+            <div className='about_me'>
+               <div className='about_me_image'>
+                  <img src='src/assets/profile.jpeg' alt='Profile Image' />
+               </div>
+
+            </div>
+            <div className='about_content'>
+
+            </div>
+
+         </div>
       </section>
    )
 }
