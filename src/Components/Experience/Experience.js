674c564349caa3dd8993fe2de1d7ccd507a02086
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-06-04 20:03:35 +1200

    set up front and backend experineces

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -1,12 +1,28 @@
 import React, { useContext } from 'react'
 import './Experience.css'
 import { ActiveNavContext } from '../../ActiveNavContext'
+import { BsPatchCheckFill } from 'react-icons/bs'
 
 
 const Experience = () => {
   const {
     setActiveNav
   } = useContext(ActiveNavContext)
+
+  const frontEnd = [
+    { language: 'JaveScript', experience: 'Intermidate' },
+    { language: 'React', experience: 'Intermidate' },
+    { language: 'CSS', experience: 'Intermidate' },
+    { language: 'HTML', experience: 'Experienced' },
+  ]
+
+  const backEnd = [
+    { language: 'Ruby', experience: 'Basic' },
+    { language: 'Node JS', experience: 'Intermidate' },
+    { language: 'MySQL', experience: 'Basic' },
+  ]
+
+
   return (
     <section
       id='experience'
@@ -19,13 +35,29 @@ const Experience = () => {
         <div className='experience_frontend'>
           <h3>Frontend Development</h3>
           <div className='experience_content'>
-
+            {frontEnd.map((info) => {
+              return (
+                <article className='experience_details'>
+                  <BsPatchCheckFill />
+                  <h4>{info.language}</h4>
+                  <small>{info.experience}</small>
+                </article>
+              )
+            })}
           </div>
         </div>
         <div className='experience_backend'>
           <h3>Backend Development</h3>
           <div className='experience_content'>
-
+            {backEnd.map((info) => {
+              return (
+                <article className='experience_details'>
+                  <BsPatchCheckFill />
+                  <h4>{info.language}</h4>
+                  <small>{info.experience}</small>
+                </article>
+              )
+            })}
           </div>
         </div>
       </div>
