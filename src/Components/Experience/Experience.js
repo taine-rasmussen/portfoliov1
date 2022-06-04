b81352ba2459c34711b7b2016f5cbab1747d54e9
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-06-04 20:18:11 +1200

    styled experience component

--- src/Components/Experience/Experience.js
+++ src/Components/Experience/Experience.js
@@ -12,7 +12,7 @@ const Experience = () => {
   const frontEnd = [
     { language: 'JaveScript', experience: 'Intermidate' },
     { language: 'React', experience: 'Intermidate' },
-    { language: 'CSS', experience: 'Intermidate' },
+    { language: 'CSS', experience: 'Experienced' },
     { language: 'HTML', experience: 'Experienced' },
   ]
 
@@ -32,29 +32,33 @@ const Experience = () => {
       <h2>My Experience</h2>
 
       <div className='container experince_container'>
-        <div className='experience_frontend'>
+        <div className='experience_skills'>
           <h3>Frontend Development</h3>
           <div className='experience_content'>
             {frontEnd.map((info) => {
               return (
                 <article className='experience_details'>
-                  <BsPatchCheckFill />
-                  <h4>{info.language}</h4>
-                  <small>{info.experience}</small>
+                  <BsPatchCheckFill className='experience_details-icon' />
+                  <div>
+                    <h4>{info.language}</h4>
+                    <small className='text-light'>{info.experience}</small>
+                  </div>
                 </article>
               )
             })}
           </div>
         </div>
-        <div className='experience_backend'>
+        <div className='experience_skills'>
           <h3>Backend Development</h3>
           <div className='experience_content'>
             {backEnd.map((info) => {
               return (
                 <article className='experience_details'>
-                  <BsPatchCheckFill />
-                  <h4>{info.language}</h4>
-                  <small>{info.experience}</small>
+                  <BsPatchCheckFill className='experience_details-icon' />
+                  <div>
+                    <h4>{info.language}</h4>
+                    <small className='text-light'>{info.experience}</small>
+                  </div>
                 </article>
               )
             })}
