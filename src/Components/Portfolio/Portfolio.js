c171a49922398b454f67b9694eba5ea9be351c8f
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-06-06 16:52:06 +1200

    create placeholder data set for projects

--- src/Components/Portfolio/Portfolio.js
+++ src/Components/Portfolio/Portfolio.js
@@ -1,12 +1,73 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './Portfolio.css'
 import IMG from '../../assets/portfolio1.jpg'
+import { ActiveNavContext } from '../../ActiveNavContext'
+
 
 // TODO:
 // Create a data object to store portfolio info in
 // Refactor component to use map on data 
 
 const Portfolio = () => {
+
+  const {
+    setActiveNav
+  } = useContext(ActiveNavContext)
+
+
+  const data = [
+    {
+      id: 1,
+      image: IMG,
+      title: 'Example project name',
+      github: 'http://github.com',
+      demo: 'http://github.com/taine-rasmussen'
+    },
+    {
+      id: 2,
+      image: IMG,
+      title: 'Example project name',
+      github: 'http://github.com',
+      demo: 'http://github.com/taine-rasmussen'
+    },
+    {
+      id: 3,
+      image: IMG,
+      title: 'Example project name',
+      github: 'http://github.com',
+      demo: 'http://github.com/taine-rasmussen'
+    },
+    {
+      id: 4,
+      image: IMG,
+      title: 'Example project name',
+      github: 'http://github.com',
+      demo: 'http://github.com/taine-rasmussen'
+    },
+    {
+      id: 5,
+      image: IMG,
+      title: 'Example project name',
+      github: 'http://github.com',
+      demo: 'http://github.com/taine-rasmussen'
+    },
+    {
+      id: 6,
+      image: IMG,
+      title: 'Example project name',
+      github: 'http://github.com',
+      demo: 'http://github.com/taine-rasmussen'
+    },
+  ]
+
+
+
+
+
+
+
+
+
   return (
     <section id='portfolio'>
       <h5>My recent work</h5>
