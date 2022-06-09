121a94fac67e40fe330b7d9c985dd1aed71e4145
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-06-09 20:04:51 +1200

    asdf

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -2,6 +2,8 @@ import React, { useContext } from 'react'
 import { ActiveNavContext } from '../../ActiveNavContext'
 import './Contact.css'
 import { MdOutlineEmail } from 'react-icons/md'
+import { BsLinkedin } from 'react-icons/bs'
+
 
 const Contact = () => {
   const {
@@ -23,6 +25,12 @@ const Contact = () => {
             <h4>Email</h4>
             <h5>taine.rasmussen@gmail.com</h5>
             <a href='taine.rasmussen@gmail.com'>Send a message</a>
+          </article>
+          <article className='contact_option'>
+            <BsLinkedin />
+            <h4>Linkedin</h4>
+            <h5>Taine Rasmussen</h5>
+            <a href='https://www.linkedin.com/in/taine-rasmussen-a66a86184/'>Send a message</a>
           </article>
         </div>
 
