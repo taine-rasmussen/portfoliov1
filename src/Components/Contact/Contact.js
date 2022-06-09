1c0e536044c7e6aefdf64333fc82cf610f702f95
Taine Rasmussen <taine.rasmussen@gmail.com>
2022-06-09 20:03:10 +1200

    started contact component????

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { ActiveNavContext } from '../../ActiveNavContext'
 import './Contact.css'
+import { MdOutlineEmail } from 'react-icons/md'
 
 const Contact = () => {
   const {
@@ -12,7 +13,20 @@ const Contact = () => {
       id='contact'
       onMouseEnter={() => { setActiveNav('contact') }}
     >
-      Contact
+      <h5>Get in touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact_container'>
+        <div className='contact_options'>
+          <article className='contact_option'>
+            <MdOutlineEmail />
+            <h4>Email</h4>
+            <h5>taine.rasmussen@gmail.com</h5>
+            <a href='taine.rasmussen@gmail.com'>Send a message</a>
+          </article>
+        </div>
+
+      </div>
     </section>
   )
 }
