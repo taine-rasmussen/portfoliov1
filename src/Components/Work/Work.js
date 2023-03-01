9cc8015a5de3032837f107587e330c7154a9b43e
Taine Rasmussen <taine.rasmussen@gmail.com>
2023-03-01 17:01:03 +0000

    i love css

--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -1,5 +1,9 @@
-import React, { useContext } from 'react'
 import { ActiveNavContext } from '../../ActiveNavContext'
+import { BsPatchCheckFill } from 'react-icons/bs'
+import React, { useContext } from 'react'
+
+import Experience from '../Experience/Experience'
+
 import './Work.css'
 
 const Work = () => {
@@ -7,12 +11,51 @@ const Work = () => {
     setActiveNav
   } = useContext(ActiveNavContext)
 
+  const skills = [
+    { language: 'JaveScript' },
+    { language: 'Redux' },
+    { language: 'MaterialUi' },
+    { language: 'React' },
+    { language: 'CSS' },
+    { language: 'HTML' },
+    { language: 'Rails' },
+    { language: 'Node.js' },
+    { language: 'SQL' },
+    { language: 'Postgres' },
+    { language: 'MongoDB' },
+    { language: 'Express' },
+  ]
+
   return (
     <section
       id='work'
       onMouseEnter={() => { setActiveNav('work') }}
+      className='work_container'
     >
+      <section className='wrapper_container'>
+        <h5>What skills I have</h5>
+        <h2>My Experience</h2>
+        <div>
+          {skills.map((skill) => (
+            <article className='work_details' key={skill.language}>
+              <h4>{skill.language}</h4>
+              <BsPatchCheckFill className='work_details-icon' />
+            </article>
+          ))}
+        </div>
+      </section>
 
+
+
+      {/* <Experience /> */}
+
+      <section className='wrapper_container'>
+        <h5>My recent work</h5>
+        <h2>Portfolio</h2>
+        <div>
+          Projects go here
+        </div>
+      </section>
     </section>
   )
 }
